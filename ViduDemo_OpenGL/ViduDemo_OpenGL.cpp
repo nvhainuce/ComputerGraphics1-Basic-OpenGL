2e7404c33389675bc58a8a6e22e77d7401ad9a44
nvhainuce <NguyenHai PC>
2021-07-17 15:24:44 +0700

    17/7

---
+++ ViduDemo_OpenGL/ViduDemo_OpenGL.cpp
@@ -0,0 +1,120 @@
+﻿// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+//#include <glm/glm.hpp>
+//using namespace glm;
+//function
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+int main(void)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Tạo 1 cửa sổ GLFWwindow 
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vi du 1 Hello Window", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+	//Đặt biến glewExperimental về true  (bắt buộc)
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Định nghĩa kích thước viewport
+	 // Define the viewport dimensions
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);    // lấy kích thước framebuffer   (chú ý)
+	glViewport(0, 0, width, height);;
+
+	////////// bước 0.1 Build và biên dịch shader program
+	////Shader ourShader("vertexShader_tamgiacNoisuy.vs", "fragmentShader_tamgiacNoisuy.frag");
+
+
+	//bước 1: khai báo vertex input (vertex data)
+	GLfloat vertices[] = {
+		// Positions         // Colors
+	   0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
+	  -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
+	   0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top 
+	};
+	//Bước 2: Khởi tạo VBO, VAO
+		//b2.1 VAO
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	//bind VAO
+	glBindVertexArray(VAO);
+	//b2.2 VBO
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // liên kết (bind) VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// postition attribute 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	//color attribute 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //unbind VBO , cho phép gọi hàm glVertexAttribPointer trong VBO
+//unbind VAO
+	glBindVertexArray(0);
+
+	//Game Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// check sự kiện  (ấn nút bàn phím, chuột,...)
+		glfwPollEvents();
+		//Render
+		//xóa color buffer
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Buoc 3 Vẽ hình 1 hình tam giác
+
+		//ourShader.Use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		///swap
+		glfwSwapBuffers(window);
+	}
+	//Terminate GLFW, xóa và dọn dẹp tài nguyên sau khi thoát
+	glfwTerminate();
+	return 0;
+
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
