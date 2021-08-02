f4bd4227266b8ade06421e59194a82fa4f5c26d8
nvhainuce <NguyenHai PC>
2021-08-02 17:56:18 +0700

    2-8-21

--- Baithuchanhso3_Shader_ClassShader/Baithuchanhso3_Shader_ClassShader.cpp
+++ Baithuchanhso3_Shader_ClassShader/Baithuchanhso3_Shader_ClassShader.cpp
@@ -1,20 +1,124 @@
-// Baithuchanhso3_Shader_ClassShader.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+// Include GLEW
+#include <GL/glew.h>
 
-int main()
+//
+#include "Shader.h"
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+
+//function
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+
+int main(void)
 {
-    std::cout << "Hello World!\n";
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Tạo 1 cửa sổ GLFWwindow 
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Bai thuc hanh so 3 - class shader", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+	//Đặt biến glewExperimental về true  (bắt buộc)
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Định nghĩa kích thước viewport
+	 // Define the viewport dimensions
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);    // lấy kích thước framebuffer   (chú ý)
+	glViewport(0, 0, width, height);;
+
+
+	Shader ourShader("vShader_b3.vertex", "fShader_b3 .frag");
+
+
+	//bước 1: khai báo vertex input (vertex data)
+	GLfloat vertices[] = {
+		// vị trí - position      ///color
+		-0.5f,-0.5f,0.0f,		1.0f,0.0f,0.0f,//bottom-left
+		0.5f,-0.5f,0.0f,	    0.0f,1.0f,0.0f,//bottom right
+		0.0f,0.5f,0.0f,	    0.0f,0.0f,1.0f//Top  
+	};
+	//Bước 2: Khởi tạo VBO, VAO
+		//b2.1 VAO
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	//bind VAO
+	glBindVertexArray(VAO);
+	//b2.2 VBO
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // liên kết (bind) VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//position attribute 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	//Color attribute 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //unbind VBO , cho phép gọi hàm glVertexAttribPointer trong VBO
+//unbind VAO
+	glBindVertexArray(0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//Game Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// check sự kiện  (ấn nút bàn phím, chuột,...)
+		glfwPollEvents();
+		//Render
+		//xóa color buffer
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Buoc 3 Vẽ hình 1 hình tam giác
+
+		ourShader.use();
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		///swap
+		glfwSwapBuffers(window);
+	}
+	//Terminate GLFW, xóa và dọn dẹp tài nguyên sau khi thoát
+	glfwTerminate();
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
