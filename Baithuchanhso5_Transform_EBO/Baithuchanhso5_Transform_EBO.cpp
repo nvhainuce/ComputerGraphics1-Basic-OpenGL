102fd298eac8e61fb5cab001059719a8cd7d081b
nvhainuce <NguyenHai PC>
2021-08-07 11:59:39 +0700

    7-8

--- Baithuchanhso5_Transform_EBO/Baithuchanhso5_Transform_EBO.cpp
+++ Baithuchanhso5_Transform_EBO/Baithuchanhso5_Transform_EBO.cpp
@@ -13,14 +13,21 @@
 
 // Include GLFW
 #include <GLFW/glfw3.h>
+
+//include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
 GLFWwindow* window;
-
-
 //function
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback_baylen(GLFWwindow* window, int key, int scancode, int action, int mode);
+
 
 const GLuint WIDTH = 800, HEIGHT = 600;
-
+float Docaothem;
 
 int main(void)
 {
@@ -43,7 +50,7 @@ int main(void)
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
-
+	glfwSetKeyCallback(window, key_callback_baylen);
 	//Đặt biến glewExperimental về true  (bắt buộc)
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -59,7 +66,7 @@ int main(void)
 	glViewport(0, 0, widthW, heightW);;
 
 	//khởi tạo shader
-	Shader ourShader("vShader_b4.vertex", "fShader_b4 .frag");
+	Shader ourShader("vShader_b5.vertex", "fShader_b5 .frag");
 
 
 	//bước 1: khai báo vertex input (vertex data)
@@ -67,7 +74,14 @@ int main(void)
 		// vị trí - position      ///color         //texture coordinate (s,t)
 		-0.5f,-0.5f,0.0f,		1.0f,0.0f,0.0f,	   0.0f,0.0f,//bottom-left
 		0.5f,-0.5f,0.0f,	    0.0f,1.0f,0.0f,    1.0f,0.0f,//bottom right
-		0.0f,0.5f,0.0f,	       0.0f,0.0f,1.0f,     0.5f,1.0f //Top  
+		0.5f,0.5f,0.0f,	       0.0f,0.0f,1.0f,     1.0f,1.0f, //Top  right
+	  -0.5f,0.5f,0.0f,	       0.0f,0.0f,1.0f,     0.0f,1.0f //Top  left
+
+
+	};
+	GLuint indices[] = {
+		0,1,2,  //tam giac 1
+		3,2,0    //tam giac 2
 	};
 	//Bước 2: Khởi tạo VBO, VAO
 		//b2.1 VAO
@@ -77,12 +91,20 @@ int main(void)
 	glBindVertexArray(VAO);
 	//b2.2 VBO
 	GLuint VBO;
+
+
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); // liên kết (bind) VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//b2.3 EBO
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // liên kết (bind) VBO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//position attribute 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8* sizeof(GL_FLOAT), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	//Color attribute 
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(float)));
@@ -100,33 +122,33 @@ int main(void)
 	GLuint texture_wood;
 	glGenTextures(1, &texture_wood);
 	glBindTexture(GL_TEXTURE_2D, texture_wood);
-		//cài đặt tham số texture wrapping
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		//cài đặt tham số texture filtering 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		//tải image, tạo texture + mipmaps
-		int width, height, nrChannels;
-		unsigned char* image = stbi_load("texture_wood2.jpg", &width, &height, &nrChannels, 0);
-		if (image)
-		{
-			///tạo mipmaps
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-		else
-		{
-			//đưa ra thông báo lỗi load image
-			std::cout << "Failed to Load texture" << std::endl;
-		}
-		//delete image 
-		stbi_image_free(image);
+	//cài đặt tham số texture wrapping
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//cài đặt tham số texture filtering 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//tải image, tạo texture + mipmaps
+	int width, height, nrChannels;
+	unsigned char* image = stbi_load("texture_wood2.jpg", &width, &height, &nrChannels, 0);
+	if (image)
+	{
+		///tạo mipmaps
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		//đưa ra thông báo lỗi load image
+		std::cout << "Failed to Load texture" << std::endl;
+	}
+	//delete image 
+	stbi_image_free(image);
 
 	//unbind texture
 	glBindTexture(GL_TEXTURE_2D, 0);  //unbind texture
-	
-	
+
+
 
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -143,12 +165,25 @@ int main(void)
 		//bind texture
 		glBindTexture(GL_TEXTURE_2D, texture_wood);
 
-		//Buoc 3 Vẽ hình 1 hình tam giác
 
+		///xác định ma trận biến đổi (các bạn muốn biến đổi đối tượng như nào )
+		glm::mat4 Trans = glm::mat4(1.0f);
+		//dịch chuyển theo x,y,z lần lượt là 0.5f,-0.5f,0.0f
+		Trans = glm::translate(Trans, glm::vec3(0.0f, -0.5f * (float)glfwGetTime() + Docaothem, 0.0f)); ///Trans=translate*Trans
+		   // scale nhỏ đi 0.5 lần
+		Trans = glm::scale(Trans, glm::vec3(0.3f, 0.3f, 0.3f)); ///Trans=translate*Trans
+			// roate 60 độ quanh trục y
+		Trans = glm::rotate(Trans, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f)); ///Trans=translate*Trans
+		GLuint UniformLocation_Transform = glGetUniformLocation(ourShader.IDProgram, "transform");
+		glUniformMatrix4fv(UniformLocation_Transform, 1, GL_FALSE, glm::value_ptr(Trans));
+
+
+
+		//Buoc 3 Vẽ hình
 		ourShader.use();
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		///swap
@@ -156,11 +191,10 @@ int main(void)
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-
+	glDeleteBuffers(1, &EBO);
 
 	//Terminate GLFW, xóa và dọn dẹp tài nguyên sau khi thoát
 	glfwTerminate();
-
 	return 0;
 
 }
@@ -169,4 +203,12 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	std::cout << key << std::endl;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+void key_callback_baylen(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+	{
+		Docaothem = Docaothem + 0.2f;
+	}
 }
