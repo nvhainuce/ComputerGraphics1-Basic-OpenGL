c29ddce8d284e3ef0edb70bcd947111f0cab9aaf
nvhainuce <NguyenHai PC>
2022-03-09 08:42:09 +0700

    9/3

--- Baithuchanhso3_Shader_ClassShader/Shader.h
+++ Baithuchanhso3_Shader_ClassShader/Shader.h
@@ -6,12 +6,15 @@
 #include <iostream>
 using namespace std;
 #include <GL/glew.h>;
+#include <glm/glm.hpp>
 
 class Shader
 {
 public:
+	//thuộc tính
 	//ID Program
 	GLuint IDProgram;
+	
 
 	//khởi tạo constructor 
 	Shader(const char* vertexPath, const char* fragmentPath)
@@ -36,7 +39,7 @@ public:
 			//close file
 			vShaderFile.close();
 			fShaderFile.close();
-			// conver stream to string
+			// chuyển đổi stream thành string
 			vertexCode = vShaderStream.str();
 			fragmentCode = fShaderStream.str();
 		}
@@ -76,10 +79,68 @@ public:
 		glUseProgram(IDProgram);
 	}
 	//uniform
-	//...
+	// utility uniform functions
+	// ------------------------------------------------------------------------
+	void setBool(const std::string& name, bool value) const
+	{
+		glUniform1i(glGetUniformLocation(IDProgram, name.c_str()), (int)value);
+	}
+	// ------------------------------------------------------------------------
+	void setInt(const std::string& name, int value) const
+	{
+		glUniform1i(glGetUniformLocation(IDProgram, name.c_str()), value);
+	}
+	// ------------------------------------------------------------------------
+	void setFloat(const std::string& name, float value) const
+	{
+		glUniform1f(glGetUniformLocation(IDProgram, name.c_str()), value);
+	}
+	// ------------------------------------------------------------------------
+	void setVec2(const std::string& name, const glm::vec2& value) const
+	{
+		glUniform2fv(glGetUniformLocation(IDProgram, name.c_str()), 1, &value[0]);
+	}
+	void setVec2(const std::string& name, float x, float y) const
+	{
+		glUniform2f(glGetUniformLocation(IDProgram, name.c_str()), x, y);
+	}
+	// ------------------------------------------------------------------------
+	void setVec3(const std::string& name, const glm::vec3& value) const
+	{
+		glUniform3fv(glGetUniformLocation(IDProgram, name.c_str()), 1, &value[0]);
+	}
+	void setVec3(const std::string& name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLocation(IDProgram, name.c_str()), x, y, z);
+	}
+	// ------------------------------------------------------------------------
+	void setVec4(const std::string& name, const glm::vec4& value) const
+	{
+		glUniform4fv(glGetUniformLocation(IDProgram, name.c_str()), 1, &value[0]);
+	}
+	void setVec4(const std::string& name, float x, float y, float z, float w)
+	{
+		glUniform4f(glGetUniformLocation(IDProgram, name.c_str()), x, y, z, w);
+	}
+	// ------------------------------------------------------------------------
+	void setMat2(const std::string& name, const glm::mat2& mat) const
+	{
+		glUniformMatrix2fv(glGetUniformLocation(IDProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	void setMat3(const std::string& name, const glm::mat3& mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(IDProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	void setMat4(const std::string& name, const glm::mat4& mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(IDProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
 private:
 	
-	//khởi tạo 1 hàm cho việc check quá trình compie,linking shader, 
+	//khởi tạo 1 hàm cho việc check quá trình compile,linking shader, 
 	void CheckCompileError(GLuint shader, std::string type)   //type: VERTEX,FRAGMENT,PROGRAM
 	{
 		int success;
